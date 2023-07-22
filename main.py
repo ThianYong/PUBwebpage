1ca48040bcc7cf2512a94d0805049f67e9fdccde
thianyong <thianyong@gmail.com>
2023-07-22 16:38:49 +0800

    test

--- main.py
+++ main.py
@@ -11,11 +11,15 @@ try:
 except FileNotFoundError:
     data = pd.DataFrame(columns=['Brand', 'Model', 'CPU Speed (MHz)', 'RAM (MB)', 'Ethernet Ports', 'Price (SGD)', 'Timestamp'])
 
-# Set the title of the app in the sidebar
-st.sidebar.title("PLC CPU Data Entry")
+# Set the title of the app
+st.title("PLC CPU Data Entry")
 
-# Input fields in the sidebar
-with st.sidebar:
+# Create two columns layout
+data_entry_column, data_display_column = st.beta_columns(2)
+
+# Data Entry Section
+with data_entry_column:
+    # Input fields
     brand = st.text_input("Enter the brand of the PLC CPU:")
     model = st.text_input("Enter the model of the PLC CPU:")
     cpu_speed = st.number_input("Enter the CPU speed (MHz):", min_value=0, step=1)
@@ -23,8 +27,10 @@ with st.sidebar:
     ethernet_ports = st.number_input("Enter the number of Ethernet ports:", min_value=0, step=1)
     price = st.number_input("Enter the price (SGD):", min_value=0, step=1)
 
-    # Save Data button
-    if st.button("Save Data"):
+    # Add a divider between data entry and display
+    st.markdown("---")
+
+    def save_data_to_csv(brand, model, cpu_speed, ram, ethernet_ports, price, data):
         # Capture current date and time
         timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
@@ -44,26 +50,29 @@ with st.sidebar:
 
         # Save the updated data to CSV on GitHub
         data.to_csv(data_dir_url + 'plc_cpu_data.csv', index=False)
+        return data
+
+    if st.button("Save Data"):
+        data = save_data_to_csv(brand, model, cpu_speed, ram, ethernet_ports, price, data)
         st.success("Data saved successfully!")
 
-# Set the main body title
-st.title("Trends in PLC CPU Prices")
+# Data Display Section
+with data_display_column:
+    # Display the entered data in a table
+    st.subheader("Entered Data:")
+    st.table(data)
 
-# Display the entered data in a table in the main body
-st.subheader("Entered Data:")
-st.table(data)
+    # Select brand and model for trend plotting
+    st.subheader("Select Brand and Model for Trend Plotting:")
+    selected_brand = st.selectbox("Select Brand:", data['Brand'].unique())
+    selected_model = st.selectbox("Select Model:", data[data['Brand'] == selected_brand]['Model'].unique())
 
-# Select brand and model for trend plotting in the main body
-st.subheader("Select Brand and Model for Trend Plotting:")
-selected_brand = st.selectbox("Select Brand:", data['Brand'].unique())
-selected_model = st.selectbox("Select Model:", data[data['Brand'] == selected_brand]['Model'].unique())
+    # Filter data based on selected brand and model
+    selected_data = data[(data['Brand'] == selected_brand) & (data['Model'] == selected_model)]
 
-# Filter data based on selected brand and model
-selected_data = data[(data['Brand'] == selected_brand) & (data['Model'] == selected_model)]
-
-# Plot the trend of price changes for the selected brand and model using native Streamlit line chart in the main body
-if not selected_data.empty:
-    st.subheader("Price Trend:")
-    st.line_chart(selected_data['Price (SGD)'])
-else:
-    st.warning("No data available for the selected brand and model.")
+    # Plot the trend of price changes for the selected brand and model using native Streamlit line chart
+    if not selected_data.empty:
+        st.subheader("Price Trend:")
+        st.line_chart(selected_data['Price (SGD)'])
+    else:
+        st.warning("No data available for the selected brand and model.")

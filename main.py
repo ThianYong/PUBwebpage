62998b235d29165dad20a22d2835cddbe4a39a8b
thianyong <thianyong@gmail.com>
2023-07-22 16:36:39 +0800

    test

--- main.py
+++ main.py
@@ -11,8 +11,8 @@ try:
 except FileNotFoundError:
     data = pd.DataFrame(columns=['Brand', 'Model', 'CPU Speed (MHz)', 'RAM (MB)', 'Ethernet Ports', 'Price (SGD)', 'Timestamp'])
 
-# Set the title of the app
-st.title("PLC CPU Data Entry")
+# Set the title of the app in the sidebar
+st.sidebar.title("PLC CPU Data Entry")
 
 # Input fields in the sidebar
 with st.sidebar:
@@ -23,40 +23,37 @@ with st.sidebar:
     ethernet_ports = st.number_input("Enter the number of Ethernet ports:", min_value=0, step=1)
     price = st.number_input("Enter the price (SGD):", min_value=0, step=1)
 
-# Add a divider between data entry and display
-st.markdown("---")
+    # Save Data button
+    if st.button("Save Data"):
+        # Capture current date and time
+        timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
-def save_data_to_csv(brand, model, cpu_speed, ram, ethernet_ports, price, data):
-    # Capture current date and time
-    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        data_entry = {
+            'Brand': brand,
+            'Model': model,
+            'CPU Speed (MHz)': cpu_speed,
+            'RAM (MB)': ram,
+            'Ethernet Ports': ethernet_ports,
+            'Price (SGD)': price,
+            'Timestamp': timestamp
+        }
 
-    data_entry = {
-        'Brand': brand,
-        'Model': model,
-        'CPU Speed (MHz)': cpu_speed,
-        'RAM (MB)': ram,
-        'Ethernet Ports': ethernet_ports,
-        'Price (SGD)': price,
-        'Timestamp': timestamp
-    }
+        # Append the data entry to the existing data
+        new_data = pd.DataFrame(data_entry, index=[0])
+        data = pd.concat([data, new_data], ignore_index=True)
 
-    # Append the data entry to the existing data
-    new_data = pd.DataFrame(data_entry, index=[0])
-    data = pd.concat([data, new_data], ignore_index=True)
+        # Save the updated data to CSV on GitHub
+        data.to_csv(data_dir_url + 'plc_cpu_data.csv', index=False)
+        st.success("Data saved successfully!")
 
-    # Save the updated data to CSV on GitHub
-    data.to_csv(data_dir_url + 'plc_cpu_data.csv', index=False)
-    return data
+# Set the main body title
+st.title("Trends in PLC CPU Prices")
 
-if st.button("Save Data"):
-    data = save_data_to_csv(brand, model, cpu_speed, ram, ethernet_ports, price, data)
-    st.success("Data saved successfully!")
-
-# Display the entered data in a table
+# Display the entered data in a table in the main body
 st.subheader("Entered Data:")
 st.table(data)
 
-# Select brand and model for trend plotting
+# Select brand and model for trend plotting in the main body
 st.subheader("Select Brand and Model for Trend Plotting:")
 selected_brand = st.selectbox("Select Brand:", data['Brand'].unique())
 selected_model = st.selectbox("Select Model:", data[data['Brand'] == selected_brand]['Model'].unique())
@@ -64,7 +61,7 @@ selected_model = st.selectbox("Select Model:", data[data['Brand'] == selected_br
 # Filter data based on selected brand and model
 selected_data = data[(data['Brand'] == selected_brand) & (data['Model'] == selected_model)]
 
-# Plot the trend of price changes for the selected brand and model using native Streamlit line chart
+# Plot the trend of price changes for the selected brand and model using native Streamlit line chart in the main body
 if not selected_data.empty:
     st.subheader("Price Trend:")
     st.line_chart(selected_data['Price (SGD)'])
